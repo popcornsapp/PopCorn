0cda8d926afe7e3bfcfe05c9f974c74bc3610085
popcorn <popcornsapp@gmail.com>
2020-07-18 16:41:37 +0530

    this is first commit

---
+++ app/src/main/java/com/videos/popcorn/Main_Menu/RelateToFragment_OnBack/OnBackPressListener.java
@@ -0,0 +1,9 @@
+package com.videos.popcorn.Main_Menu.RelateToFragment_OnBack;
+
+/**
+ * Created by AQEEL on 3/30/2018.
+ */
+
+public interface OnBackPressListener {
+    public boolean onBackPressed();
+}
---
+++ app/src/main/java/com/videos/popcorn/Video_Recording/GalleryVideos/GalleryVideo_Get_Set.java
@@ -0,0 +1,6 @@
+package com.videos.popcorn.Video_Recording.GalleryVideos;
+
+public class GalleryVideo_Get_Set {
+    String video_path,video_time;
+    long video_duration_ms;
+}
